359ce2dbe862448d8c484a527edb9b41bd682dd5
safi <safee.saqib1122@gmail.com>
2022-04-05 19:38:38 +0500

    bh

---
+++ src/components/game.jsx
@@ -0,0 +1,77 @@
+import Board from "./board";
+import React, { useState } from "react";
+
+import "../index.css";
+function Game() {
+  const [state, setState] = useState({
+    history: [
+      {
+        squares: Array(9).fill(null),
+      },
+    ],
+  });
+  const history = state.history;
+  const current = history[history.length - 1];
+  const [xIsNext, setXIsNext] = useState(true);
+  function handleClick(i) {
+    const history = state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? "X" : "O";
+    setState({
+      history: history.concat([
+        {
+          squares: squares,
+        },
+      ]),
+    });
+    setXIsNext(!xIsNext);
+  }
+  const winner = calculateWinner(current.squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          squares={current.squares}
+          onClick={(i) => {
+            handleClick(i);
+          }}
+        />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+}
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default Game;
